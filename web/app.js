76a6738553da47d0007f41948cb79e8d8095833f
eriksoftdev <eriksoftdev@gmail.com>
2025-02-07 20:50:46 -0600

    Nueva rama de Erik

--- web/app.js
+++ web/app.js
@@ -6,8 +6,6 @@ var robot4 = document.getElementById("robot4");
 var robot5 = document.getElementById("robot5");
 var robot6 = document.getElementById("robot6");
 
-// var left = document.getElementById("left");
-// var right = document.getElementById("right");
 var div = document.getElementById("disco");
 var auto = document.getElementById("auto");
 var buttonStopDance = document.getElementById("buttonStopDance");
@@ -17,76 +15,11 @@ var beat = new Audio("audio/beat.ogg");
 
 var buttonPlay = document.getElementById("play");
 
-///Este es el comentario de juan'
-//Este es el comentario de Erik
-
-/* buttonPlay.addEventListener("click", () => {
-  beat.play();
-}); */
-
 //Mover robot
-/* 
-left.addEventListener("mousedown", () => {
-  robot.src = "img/robot-l.png";
-  div.style.backgroundColor = "#FF00FF";
-});
-
-left.addEventListener("mouseup", () => {
-  positionZero();
-  div.style.backgroundColor = "#FFFF00";
-});
-
-right.addEventListener("mousedown", () => {
-  robot.src = "img/robot-r.png";
-  div.style.backgroundColor = "#33CC33";
-});
-
-right.addEventListener("mouseup", () => {
-  positionZero();
-  div.style.backgroundColor = "#FFFF00";
-});
-
-function positionZero() {
-  robot.src = "img/robot-c.png";
-} */
 
 //auto
 var srcArray = ["img/robot-l.png", "img/robot-c.png", "img/robot-r.png"];
 var colorArray = ["#FF00FF", "#FFFF00", "#33CC33"];
-
-/* auto.addEventListener("click", () => {
-  beat.play();
-  danceRobot(robot);
-  danceRobot(robot1);
-});
-
-function danceRobot(robot) {
-  let intervalDance = setInterval(() => {
-    for (let i = 0; i < colorArray.length; i++) {
-      setTimeout(function () {
-        div.style.backgroundColor = colorArray[i];
-      }, i * 350);
-    }
-    for (let i = 0; i < srcArray.length; i++) {
-      setTimeout(function () {
-        robot.src = srcArray[i];
-      }, i * 350);
-    }
-    setTimeout(() => {
-      robot.src = srcArray[1];
-    }, 1400);
-  }, 1750);
-
-  beat.addEventListener("ended", () => {
-    clearInterval(intervalDance);
-  });
-
-  stopDance.addEventListener("click", () => {
-    beat.pause();
-    clearInterval(intervalDance);
-  });
-}
- */
 
 auto.addEventListener("click", () => {
   beat.play();
@@ -115,6 +48,7 @@ buttonStopDance.addEventListener("click", () => {
   clearInterval(intervalDance);
 });
 
+//crear intervalo
 function intervalDancing(robot) {
   intervalDance = danceRobot(robot);
 
@@ -122,3 +56,4 @@ function intervalDancing(robot) {
     clearInterval(intervalDance);
   });
 }
+//
