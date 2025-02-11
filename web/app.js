55bf8d09b6ada98b9f2d0cc9d9926e2a9fd47ba8
Juan García <43949658+garcia-s@users.noreply.github.com>
2025-02-10 21:13:01 -0500

    Refactorizacion en la clase

--- web/app.js
+++ web/app.js
@@ -1,57 +1,72 @@
-var robot = document.getElementById("robot");
-var robot1 = document.getElementById("robot1");
-var robot2 = document.getElementById("robot2");
-var robot3 = document.getElementById("robot3");
-var robot4 = document.getElementById("robot4");
-var robot5 = document.getElementById("robot5");
-var robot6 = document.getElementById("robot6");
 
-var div = document.getElementById("disco");
-var auto = document.getElementById("auto");
-var buttonStopDance = document.getElementById("buttonStopDance");
+
+const robots = [
+    document.getElementById("robot"),
+    document.getElementById("robot1"),
+    document.getElementById("robot2"),
+    document.getElementById("robot3"),
+    document.getElementById("robot4"),
+    document.getElementById("robot5"),
+    document.getElementById("robot6"),
+]
+
+const div = document.getElementById("disco")
+const auto = document.getElementById("auto")
+const buttonStopDance = document.getElementById("buttonStopDance")
+
 
 //Reproducir musica
-var beat = new Audio("audio/beat.ogg");
+const buttonPlay = document.getElementById("play")
 
-var buttonPlay = document.getElementById("play");
+const srcArray = [
+    "img/robot-l.png",
+    "img/robot-c.png",
+    "img/robot-r.png",
+]
+
+const colorArray = [
+    "#FF00FF",
+    "#FFFF00",
+    "#33CC33",
+]
 
 
+let beat = new Audio("audio/beat.ogg")
+let intervalId
+let lastDancing = -1
 
-var srcArray = ["img/robot-l.png", "img/robot-c.png", "img/robot-r.png"];
-var colorArray = ["#FF00FF", "#FFFF00", "#33CC33"];
+//Cancelar la accion y iniciar una nueva
+//Comprobar si la accion ya se esta ejecutando
+
+function startDancing() {
+
+    let i = 0
+    return setInterval(function() {
+        div.style.backgroundColor = colorArray[i]
+        for (let j = 0; j <= lastDancing; j++) {
+            robots[j].src = srcArray[i]
+        }
+        i = (i + 1) % colorArray.length
+    }, 1000)
+}
 
 
 auto.addEventListener("click", () => {
-    beat.play();
-    intervalDancing(robot);
-    intervalDancing(robot1);
-    intervalDancing(robot2);
-    intervalDancing(robot3);
-    intervalDancing(robot4);
-    intervalDancing(robot5);
-    intervalDancing(robot6);
-});
+    intervalId = intervalId ?? startDancing()
+    if (lastDancing < robots.length - 1) lastDancing++
+    beat.play()
+})
 
-function danceRobot(robot) {
-    //ring buffer
-    let i = 0;
-    return setInterval(function() {
-        div.style.backgroundColor = colorArray[i];
-        robot.src = srcArray[i];
-        i = (i + 1) % colorArray.length;
-    }, 1000);
-}
-let intervalDance;
+
+beat.addEventListener("ended", () => clearInterval(intervalId))
+
+
+
 buttonStopDance.addEventListener("click", () => {
-    beat.pause();
-    beat.currentTime = 0;
-    clearInterval(intervalDance);
-});
+    beat.pause()
+    beat.currentTime = 0
+    clearInterval(intervalId)
+    intervalId = null
+    lastDancing = -1
+})
 
-function intervalDancing(robot) {
-    intervalDance = danceRobot(robot);
-
-    beat.addEventListener("ended", () => {
-        clearInterval(intervalDance);
-    });
-}
